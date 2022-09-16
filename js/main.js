9d9b35b52986e8a8ea192ada7f81b5e9898f9360
kangdayeon <dykang@ui-lab.co.kr>
2022-09-16 14:34:22 +0900

    scroll 아이콘 추가, 3번째 섹션 배경 컬러 변하는 함수 추가

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
-const input = $('.name-input');
-const messageBox = $('.message');
-const userName = $('.user-name');
+$(document).ready(function () {
+  // aos적용
+  AOS.init();
 
-$('.name-input').keydown(function (e) {
-  if (e.keyCode == 13) {
-    let inputValue = input.val();
-    userName.text(inputValue);
-    input.removeClass('active');
-    messageBox.addClass('active');
-  }
+  const input = $('.name-input');
+  const messageBox = $('.message');
+  const userName = $('.user-name');
+
+  $('.name-input').keydown(function (e) {
+    if (e.keyCode == 13) {
+      let inputValue = input.val();
+      userName.text(inputValue);
+      input.removeClass('active');
+      messageBox.addClass('active');
+    }
+  });
+
+  $(window).scroll(function () {
+    let scrollTop = $(document).scrollTop();
+    let start = $('#section-bg').offset().top;
+    if (scrollTop == start) {
+      $('#section-bg').css('background-color', '#00acc1')
+    } else if (scrollTop < start) {
+      $('#section-bg').css('background-color', '#fff')
+    }
+  });
 });
