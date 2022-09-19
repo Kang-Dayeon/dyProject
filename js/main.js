72338a0e26a163972765ad4a34ce39df52e5b3ab
kangdayeon <dykang@ui-lab.co.kr>
2022-09-19 14:00:04 +0900

    tween max 효과 추가

--- js/main.js
+++ js/main.js
@@ -16,5 +16,31 @@ $(document).ready(function () {
   });
   $('.top').click(function () {
     $('html,body').animate({ scrollTop: 0 }, 500);
-  })
+  });
+
+  $('#mouseStart').mousemove(function (e) {
+    $('.cursor').css('top', e.clientY);
+    $('.cursor').css('left', e.clientX);
+  });
+
+  $('#mouseStart').click(function (e) {
+    gsap.fromTo('.circle-click', 3, {
+      rotation: 0,
+      x: e.clientX,
+      y: e.clientY,
+      width: 10,
+      height: 10,
+      borderRadius: 0,
+      ease: Back.easeOut.config(1),
+    },
+      {
+        rotation: 360,
+        x: e.clientX,
+        y: e.clientY,
+        width: 400,
+        height: 400,
+        ease: Back.easeOut.config(1),
+        borderRadius: 50,
+      });
+  });
 });
